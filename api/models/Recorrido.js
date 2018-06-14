7d78593923c47eb379924dc6d768f0b6f112192a
magallegos1996 <bwcekos@gmail.com>
2018-06-14 11:53:09 -0500

    Modelo Recorrido (sin relaciones)

---
+++ api/models/Recorrido.js
@@ -0,0 +1,51 @@
+/**
+ * Recorrido.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    origenRecorrido:{
+      type:'string',
+      requiered:true,
+    },
+
+    destinoRecorrido:{
+      type:'string',
+      requiered:true,
+    },
+
+    distanciaRecorrido:{
+      type:'number',
+      requiered: true,
+    },
+
+    estadoRecorrido:{
+      type:'boolean',
+      requiered:true,
+    },
+
+    fechaRecorrido:{
+      type:'string',
+      requiered:true
+    },
+    valorRecorrido:{
+      type:'number',
+      requiered:true
+    },
+
+
+
+
+
+
+
+
+  },
+
+};
+
