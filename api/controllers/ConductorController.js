d2ab9cf659172a72cfe9d7546789767acc44b2f3
Daniel Aucatoma <danielaucatoma1996@hotmail.com>
2018-07-01 20:50:10 -0500

    Se borra líneas de código de prueba

--- api/controllers/ConductorController.js
+++ api/controllers/ConductorController.js
@@ -8,16 +8,5 @@
 
 module.exports = {
 
-  _config :{
-    actions: false,
-    rest: false,
-  },
-
-  login: async function(req, res){
-      var resultado = await sails.helpers.fooBar('Daniel');
-      var conductor = await Cliente.find();
-      res.send(conductor)
-  }
-
 };
 
